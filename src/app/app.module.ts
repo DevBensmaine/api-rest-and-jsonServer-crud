c6e089fbec18f45d2bef5f0a613387a3da707bf7
DevBensmaine <bensmaine.hassan@gmail.com>
2020-07-11 22:31:22 +0100

    Create Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommandeComponent } from './components/commande/commande.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommandeComponent
   ],
   imports: [
     BrowserModule
